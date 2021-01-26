3b393fcd78d860dbcd142853a38c25083505dfc9
aaaasmile <testhaus@gmx.at>
2021-01-26 20:50:54 +0100

    start to turn on off display

--- dh11HumDisplnano/dh11HumDisplnano.ino
+++ dh11HumDisplnano/dh11HumDisplnano.ino
@@ -17,20 +17,31 @@ LiquidCrystal_I2C lcd(0x27,16,2); // set the LCD address to 0x27 for a 16 chars 
 //      VCC: 5V or 3V
 //      GND: GND
 //      DATA: 2
-int pinDHT11 = 2;
+const int pinDHT11 = 2;
+const int buttonApin = 9;
 SimpleDHT11 dht11;
 
 String g_strLine1;
 String g_strLine2;
 char buff1[32];
 char buff2[32];
-int prevTemp;
-int prevHum;
+int g_prevTemp = 0;
+int g_prevHum = 0;
+int g_lightloop = 0;
+int g_readLoop = 0;
+const int g_defDelay = 100;
+const int sec_with_light = 5;
+const int sec_between_read = 3;
+const int turnOffLoop = sec_with_light * 1000 / g_defDelay; 
+const int readLoop = sec_between_read  * 1000 / g_defDelay;
 
 #define DEBUG
 
 void setup() {
   lcd.init();
+  
+  pinMode(buttonApin, INPUT_PULLUP);
+
   #ifdef DEBUG
   Serial.begin(9600);
   #endif
@@ -42,57 +53,77 @@ void loop() {
   Serial.println("=================================");
   Serial.println("Sample DHT11...");
   #endif
-  
-  // read with raw sample data.
-  byte temperature = 0;
-  byte humidity = 0;
-  byte data[40] = {0};
-  if (dht11.read(pinDHT11, &temperature, &humidity, data)) {
-    #ifdef DEBUG
-    Serial.print("Read DHT11 failed\n");
-    #endif
-    delay(1000);
-    return;
-  }
-  int currTemp = (int)temperature;
-  int currHumidity = (int)humidity;
+  int currTemp = g_prevTemp;
+  int currHumidity = g_prevHum;
 
-  #ifdef DEBUG
-  Serial.print("Read OK, Sample RAW Bits: ");
-  for (int i = 0; i < 40; i++) {
-    Serial.print((int)data[i]);
-    if (i > 0 && ((i + 1) % 4) == 0) {
-      Serial.print(' ');
+  if (g_readLoop >= readLoop){
+    g_readLoop = 0;
+    // read with raw sample data.
+    byte temperature = 0;
+    byte humidity = 0;
+    byte data[40] = {0};
+    if (dht11.read(pinDHT11, &temperature, &humidity, data)) {
+      #ifdef DEBUG
+      Serial.print("Read DHT11 failed\n");
+      #endif
+      delay(g_defDelay);
+      g_readLoop ++;
+      return;
     }
+    currTemp = (int)temperature;
+    currHumidity = (int)humidity;
+
+    #ifdef DEBUG
+    Serial.print("Read OK, Sample RAW Bits: ");
+    for (int i = 0; i < 40; i++) {
+      Serial.print((int)data[i]);
+      if (i > 0 && ((i + 1) % 4) == 0) {
+        Serial.print(' ');
+      }
+    }
+    Serial.println("");
+    Serial.print("Sample OK: ");
+    Serial.print(currTemp); Serial.print(" *C, ");
+    Serial.print(currHumidity); Serial.println(" %");
+    #endif
+    
+    snprintf(buff1, sizeof(buff1), "%d C Temperature", currTemp);
+    g_strLine1 = buff1;
+    snprintf(buff2, sizeof(buff2), "%d %% Humidity", currHumidity);
+    g_strLine2 = buff2;
+  }else{
+    g_readLoop++;
   }
-  Serial.println("");
-  Serial.print("Sample OK: ");
-  Serial.print(currTemp); Serial.print(" *C, ");
-  Serial.print(currHumidity); Serial.println(" %");
-  #endif
   
-  
-  snprintf(buff1, sizeof(buff1), "%d C Temperature", currTemp);
-  g_strLine1 = buff1;
-  snprintf(buff2, sizeof(buff2), "%d %% Humidity", currHumidity);
-  g_strLine2 = buff2;
-  
-  if (prevTemp != currTemp || prevHum != currHumidity){
+  if (g_prevTemp != currTemp || g_prevHum != currHumidity){
     lcd.backlight();
     lcd.setCursor(0,0);
     lcd.print(g_strLine1);
     lcd.setCursor(0,1);
     lcd.print(g_strLine2);
-    prevTemp = currTemp;
-    prevHum = currHumidity;
+    g_prevTemp = currTemp;
+    g_prevHum = currHumidity;
   }else{
-    lcd.noBacklight();
-    #ifdef DEBUG
-    Serial.println("No backlight");
-    #endif
+    if (g_lightloop = turnOffLoop){
+      g_lightloop ++;
+      lcd.noBacklight();
+      #ifdef DEBUG
+      Serial.println("Turn off display");
+      #endif
+    }else if (g_lightloop < turnOffLoop){
+      g_lightloop ++;
+    }
+    
+    if (digitalRead(buttonApin) == LOW){
+      #ifdef DEBUG
+      Serial.println("Turn on light");
+      #endif
+      lcd.backlight();
+      g_lightloop = 0;
+    }
   }
- 
+
   // DHT11 sampling rate is 1HZ.
-  delay(3000);
+  delay(g_defDelay);
 
 }
